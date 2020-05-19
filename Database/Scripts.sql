1a067bb262cb7bc1eda1e7e8460fa67433a060e9
Hall-Nathan <nathanhall13@hotmail.com>
2020-05-19 11:38:07 +0100

    Insert statement added

--- Database/Scripts.sql
+++ Database/Scripts.sql
@@ -7,34 +7,33 @@ VALUES('Good'),
 	  ('Anti-hero')
 
 
-SELECT * FROM dbo.Affinity;
-
-DROP TABLE IF EXISTS dbo.Superhero;
-
 INSERT INTO dbo.Superhero (AffinityID, Name)
 VALUES(1, 'Ironman'),
 	  (2,'Captain America')
-	  
-SELECT * FROM Ability;
+	 
 
 INSERT INTO dbo.Ability(Name, Description)
 VALUES('X ray vision','Can see through stuff'),
 	('Super Strength','Can lift heavy stuff'),
 	('Flight', 'Can fly')
 
+INSERT INTO dbo.Region(Name)
+VALUES('CANADA'),
+	  ('MURICUH')
+
 Insert INTO dbo.SuperheroRegion (SuperheroID, RegionID)
-VALUES (5, 1),
-	   (6, 1)
+VALUES (1, 1),
+	   (2, 1)
 
 INSERT INTO dbo.SuperheroAbility (SuperheroID, AbilityID)
-VALUES (5,1),
-	   (6,1),
-	   (6,2),
-	   (5,3)
+VALUES (1,1),
+	   (2,1),
+	   (2,2),
+	   (1,3)
 
 INSERT INTO dbo.Background (SuperheroID)
-VALUES (5),
-       (6);
+VALUES (1),
+       (2);
 
 INSERT INTO dbo.Origin (Type)
 VALUES ('Mutation'),
@@ -48,5 +47,4 @@ INSERT INTO dbo.Alias (BackgroundID, Firstname, Lastname)
 VALUES (1, 'Tony', 'Stark'),
 	   (2, 'Peter', 'Parker')
 
-SELECT * FROM Background;
-
+SELECT * FROM ability;
