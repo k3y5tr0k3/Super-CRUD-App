8b9d0629946c3494ddf6bdd77c9133a008e18c7a
Hall-Nathan <nathanhall13@hotmail.com>
2020-05-19 15:53:17 +0100

    Add / Update Form added

---
+++ Super-CRUD-App/Windows/AddUpdateWindow/AddUpdateForm.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Super_CRUD_App.Windows.AddUpdateWindow
+{
+    partial class AddUpdateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAlias = new System.Windows.Forms.Label();
+            this.lblAffinity = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxAlias = new System.Windows.Forms.TextBox();
+            this.textBoxAffinity = new System.Windows.Forms.TextBox();
+            this.lblRegion = new System.Windows.Forms.Label();
+            this.lblPowerOrigin = new System.Windows.Forms.Label();
+            this.lblAbility = new System.Windows.Forms.Label();
+            this.lblAbilityDescription = new System.Windows.Forms.Label();
+            this.textBoxRegion = new System.Windows.Forms.TextBox();
+            this.textBoxPowerOrigin = new System.Windows.Forms.TextBox();
+            this.textBoxAbility = new System.Windows.Forms.TextBox();
+            this.textBoxAbilityDescription = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(69, 82);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(45, 17);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lblAlias
+            // 
+            this.lblAlias.AutoSize = true;
+            this.lblAlias.Location = new System.Drawing.Point(69, 152);
+            this.lblAlias.Name = "lblAlias";
+            this.lblAlias.Size = new System.Drawing.Size(38, 17);
+            this.lblAlias.TabIndex = 0;
+            this.lblAlias.Text = "Alias";
+            // 
+            // lblAffinity
+            // 
+            this.lblAffinity.AutoSize = true;
+            this.lblAffinity.Location = new System.Drawing.Point(69, 214);
+            this.lblAffinity.Name = "lblAffinity";
+            this.lblAffinity.Size = new System.Drawing.Size(50, 17);
+            this.lblAffinity.TabIndex = 0;
+            this.lblAffinity.Text = "Affinity";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(276, 79);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(408, 22);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // textBoxAlias
+            // 
+            this.textBoxAlias.Location = new System.Drawing.Point(276, 149);
+            this.textBoxAlias.Name = "textBoxAlias";
+            this.textBoxAlias.Size = new System.Drawing.Size(408, 22);
+            this.textBoxAlias.TabIndex = 1;
+            // 
+            // textBoxAffinity
+            // 
+            this.textBoxAffinity.Location = new System.Drawing.Point(276, 211);
+            this.textBoxAffinity.Name = "textBoxAffinity";
+            this.textBoxAffinity.Size = new System.Drawing.Size(408, 22);
+            this.textBoxAffinity.TabIndex = 1;
+            // 
+            // lblRegion
+            // 
+            this.lblRegion.AutoSize = true;
+            this.lblRegion.Location = new System.Drawing.Point(69, 285);
+            this.lblRegion.Name = "lblRegion";
+            this.lblRegion.Size = new System.Drawing.Size(53, 17);
+            this.lblRegion.TabIndex = 0;
+            this.lblRegion.Text = "Region";
+            this.lblRegion.UseMnemonic = false;
+            // 
+            // lblPowerOrigin
+            // 
+            this.lblPowerOrigin.AutoSize = true;
+            this.lblPowerOrigin.Location = new System.Drawing.Point(69, 356);
+            this.lblPowerOrigin.Name = "lblPowerOrigin";
+            this.lblPowerOrigin.Size = new System.Drawing.Size(89, 17);
+            this.lblPowerOrigin.TabIndex = 0;
+            this.lblPowerOrigin.Text = "Power Origin";
+            // 
+            // lblAbility
+            // 
+            this.lblAbility.AutoSize = true;
+            this.lblAbility.Location = new System.Drawing.Point(69, 425);
+            this.lblAbility.Name = "lblAbility";
+            this.lblAbility.Size = new System.Drawing.Size(45, 17);
+            this.lblAbility.TabIndex = 0;
+            this.lblAbility.Text = "Ability";
+            // 
+            // lblAbilityDescription
+            // 
+            this.lblAbilityDescription.AutoSize = true;
+            this.lblAbilityDescription.Location = new System.Drawing.Point(69, 489);
+            this.lblAbilityDescription.Name = "lblAbilityDescription";
+            this.lblAbilityDescription.Size = new System.Drawing.Size(120, 17);
+            this.lblAbilityDescription.TabIndex = 0;
+            this.lblAbilityDescription.Text = "Ability Description";
+            // 
+            // textBoxRegion
+            // 
+            this.textBoxRegion.Location = new System.Drawing.Point(276, 282);
+            this.textBoxRegion.Name = "textBoxRegion";
+            this.textBoxRegion.Size = new System.Drawing.Size(408, 22);
+            this.textBoxRegion.TabIndex = 1;
+            // 
+            // textBoxPowerOrigin
+            // 
+            this.textBoxPowerOrigin.Location = new System.Drawing.Point(276, 353);
+            this.textBoxPowerOrigin.Name = "textBoxPowerOrigin";
+            this.textBoxPowerOrigin.Size = new System.Drawing.Size(408, 22);
+            this.textBoxPowerOrigin.TabIndex = 1;
+            // 
+            // textBoxAbility
+            // 
+            this.textBoxAbility.Location = new System.Drawing.Point(276, 422);
+            this.textBoxAbility.Name = "textBoxAbility";
+            this.textBoxAbility.Size = new System.Drawing.Size(408, 22);
+            this.textBoxAbility.TabIndex = 1;
+            // 
+            // textBoxAbilityDescription
+            // 
+            this.textBoxAbilityDescription.Location = new System.Drawing.Point(276, 486);
+            this.textBoxAbilityDescription.Multiline = true;
+            this.textBoxAbilityDescription.Name = "textBoxAbilityDescription";
+            this.textBoxAbilityDescription.Size = new System.Drawing.Size(408, 80);
+            this.textBoxAbilityDescription.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(183, 598);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 36);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(363, 598);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 36);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(553, 598);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 36);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // AddUpdateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 657);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBoxAbilityDescription);
+            this.Controls.Add(this.textBoxAbility);
+            this.Controls.Add(this.textBoxPowerOrigin);
+            this.Controls.Add(this.textBoxAffinity);
+            this.Controls.Add(this.textBoxRegion);
+            this.Controls.Add(this.textBoxAlias);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.lblAbilityDescription);
+            this.Controls.Add(this.lblAbility);
+            this.Controls.Add(this.lblPowerOrigin);
+            this.Controls.Add(this.lblAffinity);
+            this.Controls.Add(this.lblRegion);
+            this.Controls.Add(this.lblAlias);
+            this.Controls.Add(this.lblName);
+            this.Name = "AddUpdateForm";
+            this.Text = "Add / Update Superhero";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAlias;
+        private System.Windows.Forms.Label lblAffinity;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxAlias;
+        private System.Windows.Forms.TextBox textBoxAffinity;
+        private System.Windows.Forms.Label lblRegion;
+        private System.Windows.Forms.Label lblPowerOrigin;
+        private System.Windows.Forms.Label lblAbility;
+        private System.Windows.Forms.Label lblAbilityDescription;
+        private System.Windows.Forms.TextBox textBoxRegion;
+        private System.Windows.Forms.TextBox textBoxPowerOrigin;
+        private System.Windows.Forms.TextBox textBoxAbility;
+        private System.Windows.Forms.TextBox textBoxAbilityDescription;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Super-CRUD-App/Windows/AddUpdateWindow/AddUpdateForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Super_CRUD_App.Windows.AddUpdateWindow
+{
+    public partial class AddUpdateForm : Form
+    {
+        public AddUpdateForm()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
